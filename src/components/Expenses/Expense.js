da1219f95c7a5e792faea48b9797256c6cc9cb23
Pooja Harugop <Pooja.Harugop@BRILLIO.COM>
2023-01-04 15:03:07 +0530

    first commit

---
+++ src/components/Expenses/Expense.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import ExpenseItem from "./ExpenseItem";
+import "./Expense.css";
+import Card from "../UI/Card";
+import ExpenseFilter from "./ExpenseFilter";
+
+function Expenses({ expenses }) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpenseFilter
+          onChangeFilter={filterChangeHandler}
+          selected={filteredYear}
+        />
+
+        {expenses.map((expense) => {
+          return (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          );
+        })}
+      </Card>
+    </div>
+  );
+}
+
+export default Expenses;
